f6eab3c0fb45e156f4428064fc385129764e7a83
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-01-10 10:53:33 -0500

    Updated to PHP 7.3

--- src/SoapClientInterface.php
+++ src/SoapClientInterface.php
@@ -11,35 +11,47 @@ interface SoapClientInterface
      * @param array  $arguments         An array of the arguments to pass to the function.
      * @return mixed                    A promise object whose behavior conforms to https://promisesaplus.com.
      */
-    public function __call($name, $arguments);
+    public function __call(string $name, array $arguments);
 
     /**
      * Synchronous SOAP call.
      *
-     * @param string $name              The name of the SOAP function to call.
-     * @param array  $arguments         An array of the arguments to pass to the function.
-     * @param array  $options           An associative array of options to pass to the client.
+     * @param string     $name          The name of the SOAP function to call.
+     * @param array      $arguments     An array of the arguments to pass to the function.
+     * @param array|null $options       An associative array of options to pass to the client.
      *                                  The location option is the URL of the remote Web service.
      *                                  The uri option is the target namespace of the SOAP service.
      *                                  The soapaction option is the action to call.
-     * @param mixed  $inputHeaders      An array of headers to be sent along with the SOAP request.
-     * @param array  $outputHeaders     If supplied, this array will be filled with the headers from the SOAP response.
+     * @param array|null $inputHeaders  An array of headers to be sent along with the SOAP request.
+     * @param array|null $outputHeaders If supplied, this array will be filled with the headers from the SOAP response.
      * @return mixed                    The eventual value of promise returned from callAsync.
      */
-    public function call($name, array $arguments, array $options = null, $inputHeaders = null, array &$outputHeaders = null);
+    public function call(
+        string $name,
+        array $arguments,
+        array $options = null,
+        array $inputHeaders = null,
+        array &$outputHeaders = null
+    );
 
     /**
      * Asynchronous SOAP call.
      *
-     * @param string $name              The name of the SOAP function to call.
-     * @param array  $arguments         An array of the arguments to pass to the function.
-     * @param array  $options           An associative array of options to pass to the client.
+     * @param string     $name          The name of the SOAP function to call.
+     * @param array      $arguments     An array of the arguments to pass to the function.
+     * @param array|null $options       An associative array of options to pass to the client.
      *                                  The location option is the URL of the remote Web service.
      *                                  The uri option is the target namespace of the SOAP service.
      *                                  The soapaction option is the action to call.
-     * @param mixed  $inputHeaders      An array of headers to be sent along with the SOAP request.
-     * @param array  $outputHeaders     If supplied, this array will be filled with the headers from the SOAP response.
+     * @param array|null $inputHeaders  An array of headers to be sent along with the SOAP request.
+     * @param array|null $outputHeaders If supplied, this array will be filled with the headers from the SOAP response.
      * @return mixed                    A promise object whose behavior conforms to https://promisesaplus.com.
      */
-    public function callAsync($name, array $arguments, array $options = null, $inputHeaders = null, array &$outputHeaders = null);
+    public function callAsync(
+        string $name,
+        array $arguments,
+        array $options = null,
+        $inputHeaders = null,
+        array &$outputHeaders = null
+    );
 }
